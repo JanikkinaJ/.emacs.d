461e5233ab9fc9407d03fa1968d42a2818cf5051
janik <27766253+JanikkinaJ@users.noreply.github.com>
2024-02-16 00:56:23 +0100

    many changes, such as magit, fixing magit spc issue

--- init.el
+++ init.el
@@ -18,5 +18,19 @@
 ;; use straight automatically when using use-package 
 (setq straight-use-package-by-default t)
 
-
+;; load actual config
 (load (locate-user-emacs-file "config.el"))
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-hook 'after-save-hook #'org-babel-tangle nil t))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
